ba13dfdde6e8a98676b6152519ac901f3967783c
Juan Miguel Sánchez Begines <jumismo@uoc.edu>
2019-10-13 22:36:58 +0200

    Se pasan datos a la vista con Model

--- spring-boot-web/src/main/java/com/jumismo/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/jumismo/springboot/web/app/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.jumismo.springboot.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,9 +11,12 @@ public class IndexController {
 	// Si se omite el method, por defecto es del tipo GET
 	//@RequestMapping(value = "/index", method = RequestMethod.GET)
 	// Si no tengo mas parametros puedo quitar el value
+	// Se puede usar Model, ModelMap, ModelAndView o Map para pasar parámetros a la vista
+	// Con ModelAndView, el método devuelve ese tipo y se le pasa la vista con setViewName("nombrevista")
 	@GetMapping(value = {"/index", "/", "home"})
-	public String index()
+	public String index(Model model)
 	{
+		model.addAttribute("titulo", "Hola Spring Framework!");
 		return "index";
 	}
 
