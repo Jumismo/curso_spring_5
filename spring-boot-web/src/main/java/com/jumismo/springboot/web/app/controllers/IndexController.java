c0de57af7eaba43ee4d292178d132b2ec1cb6ed1
Juan Miguel Sánchez Begines <jumismo@uoc.edu>
2019-10-13 23:00:12 +0200

    Se añade el concepto de Model (POJO)

--- spring-boot-web/src/main/java/com/jumismo/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/jumismo/springboot/web/app/controllers/IndexController.java
@@ -3,8 +3,12 @@ package com.jumismo.springboot.web.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jumismo.springboot.web.app.models.Usuario;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 	
 	
@@ -13,11 +17,23 @@ public class IndexController {
 	// Si no tengo mas parametros puedo quitar el value
 	// Se puede usar Model, ModelMap, ModelAndView o Map para pasar parámetros a la vista
 	// Con ModelAndView, el método devuelve ese tipo y se le pasa la vista con setViewName("nombrevista")
-	@GetMapping(value = {"/index", "/", "home"})
+	@GetMapping(value = {"/index", "/", "", "home"})
 	public String index(Model model)
 	{
-		model.addAttribute("titulo", "Hola Spring Framework!");
+		model.addAttribute("titulo", "Hola Spring Framework con Model!");
 		return "index";
 	}
 
+	
+	@RequestMapping(value = "/perfil")
+	public String perfil(Model model)
+	{
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Juanmi");
+		usuario.setApellido("Apellido");
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+
+		return "perfil";
+	}
 }
